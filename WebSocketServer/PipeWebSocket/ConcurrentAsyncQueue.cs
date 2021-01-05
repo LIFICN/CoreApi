d6070eeb4be98c6f28f79c003b4a62f37bb47aaf
LI <123456@qq.com>
2021-01-05 19:21:30 +0800

    add ConcurrentAsyncQueue

---
+++ WebSocketServer/PipeWebSocket/ConcurrentAsyncQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PipeWebSocket
+{
+    public class ConcurrentAsyncQueue<T>
+    {
+        private readonly ConcurrentQueue<T> queue = null;
+        public Action<T> OnProcess = null;
+        public Action<T, Exception> OnException = null;
+        private int isProcessing;   //队列是否正在处理数据
+        private const int Processing = 1;   //有线程正在处理数据
+        private const int UnProcessing = 0;   //没有线程处理数据
+        private CancellationTokenSource cts = null;  //线程退出令牌
+        public int MaxCount { get; }  //队列最大容量
+        public int ThreadCount { get; }  //消费线程数量
+
+        public ConcurrentAsyncQueue(int threadCount = 1, int maxCount = int.MaxValue)
+        {
+            queue = new ConcurrentQueue<T>();
+            ThreadCount = threadCount;
+            MaxCount = maxCount;
+        }
+
+        public bool IsRunning
+        {
+            get => Interlocked.CompareExchange(ref isProcessing, Processing, UnProcessing) == Processing;
+        }
+
+        public int Count { get => queue.Count; }
+
+        public void Push(T obj)
+        {
+            if (queue.Count >= MaxCount) return;
+
+            queue.Enqueue(obj);
+
+            if (!IsRunning)
+                CreateTasks();
+        }
+
+        private void Listening()
+        {
+            while (queue.Count > 0)
+            {
+                if (queue.TryDequeue(out T item))
+                {
+                    try
+                    {
+                        OnProcess?.Invoke(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        OnException?.Invoke(item, ex);
+                    }
+                }
+            }
+        }
+
+        private async void CreateTasks()
+        {
+            cts = new CancellationTokenSource();
+            List<Task> tasks = new List<Task>(ThreadCount);
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                var task = Task.Factory.StartNew(() => Listening(), cts.Token);
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);  //等待消费线程执行完成
+            ResetState(); //重置状态
+            cts.Cancel();  //结束线程池
+            tasks.Clear(); //清理线程池
+        }
+
+        public void ResetAll()
+        {
+            if (queue == null || cts == null) return;
+
+            ResetState();
+            queue.Clear();
+            cts.Cancel();
+        }
+
+        private void ResetState() => _ = Interlocked.Exchange(ref isProcessing, UnProcessing);
+    }
+}
